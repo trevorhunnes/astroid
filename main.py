554e0b41be29f5283c02e478f0eb6b40f8b135dd
harimang <thunnes@hotmail.com>
2025-07-07 18:10:08 -0700

    added player character

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player1 = Player(x, y)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         dt = (clock.tick(60)) / 2
 
