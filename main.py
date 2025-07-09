326c625563898afb2fa78ffc2130c3120cba0b5c
harimang <thunnes@hotmail.com>
2025-07-09 08:14:07 -0700

    added players vactor movement and groups

--- main.py
+++ main.py
@@ -10,16 +10,22 @@ def main():
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player1 = Player(x, y)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+
+        updatable.update(dt)
         screen.fill("black")
-        player1.draw(screen)
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
-        dt = (clock.tick(60)) / 2
-        player1.update(dt)
+        dt = (clock.tick(60)) / 100
 
 
 if __name__ == "__main__":
