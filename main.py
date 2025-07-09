53136bc83f1dcde2e06cf13edf96f7404b8dce20
harimang <thunnes@hotmail.com>
2025-07-09 08:57:58 -0700

    fixed player move speed

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def main():
         for item in drawable:
             item.draw(screen)
         pygame.display.flip()
-        dt = (clock.tick(60)) / 100
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
